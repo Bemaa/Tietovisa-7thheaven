e8e5b338dbb63886ad87971c56a7d04524ca9697
Joni Lampela <93704834+LastJonicorn@users.noreply.github.com>
2021-11-24 17:35:57 +0200

    Luonnontiede.Joni

---
+++ Luonnontiede/Luonnontiede.Joni.js
@@ -0,0 +1,8 @@
+function anwser(){
+    if(document.getElementById("vastaus3").checked){
+        window.location.replace("../../Tietovisa-7thheaven/Luonnontiede/infoSivut/LuonnontiedeHirvi.Joni.html")
+    }
+    else{
+        alert("Väärin meni!")
+    }
+};
