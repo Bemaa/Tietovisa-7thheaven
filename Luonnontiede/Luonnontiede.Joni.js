8bef319cd2072fbe4a68af8e4243259cd766538d
LastJonicorn <joni.k.lampela@gmail.com>
2021-11-25 12:29:10 +0200

    Selitesivut lisätty Luonnontieteeseen

--- Luonnontiede/Luonnontiede.Joni.js
+++ Luonnontiede/Luonnontiede.Joni.js
@@ -1,8 +1,17 @@
 function anwser(){
+    if(document.getElementById("vastaus1").checked){
+        window.location.replace("../../Tietovisa-7thheaven/Luonnontiede/infoSivut/LuonnontiedeHirvi.Joni.html")
+    }
+    if(document.getElementById("vastaus2").checked){
+        window.location.replace("../../Tietovisa-7thheaven/Luonnontiede/infoSivut/LuonnontiedeIlves.Joni.html")
+    }
     if(document.getElementById("vastaus3").checked){
         window.location.replace("../../Tietovisa-7thheaven/Luonnontiede/infoSivut/LuonnontiedeTiikeri.Joni.html")
     }
-    else{
-        alert("Väärin meni!")
+    if(document.getElementById("vastaus4").checked){
+        window.location.replace("../../Tietovisa-7thheaven/Luonnontiede/infoSivut/LuonnontiedeHuuhkaja.Joni.html")
+    }
+    if(document.getElementById("vastaus5").checked){
+        window.location.replace("../../Tietovisa-7thheaven/Luonnontiede/infoSivut/LuonnontiedeKettu.Joni.html")
     }
 };
