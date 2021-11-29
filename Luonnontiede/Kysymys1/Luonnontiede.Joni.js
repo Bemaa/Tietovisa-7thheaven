1c858a1a82d35eef790677e9f799d7ab2915c87b
LastJonicorn <joni.k.lampela@gmail.com>
2021-11-29 14:16:20 +0200

    Luotu kysymys1

---
+++ Luonnontiede/Kysymys1/Luonnontiede.Joni.js
@@ -0,0 +1,17 @@
+function anwser(){
+    if(document.getElementById("vastaus1").checked){
+        window.location.replace("../../Luonnontiede/Kysymys1/infoSivut/LuonnontiedeHirvi.Joni.html")
+    }
+    if(document.getElementById("vastaus2").checked){
+        window.location.replace("../../Luonnontiede/Kysymys1/infoSivut/LuonnontiedeIlves.Joni.html")
+    }
+    if(document.getElementById("vastaus3").checked){
+        window.location.replace("../../Luonnontiede/Kysymys1/infoSivut/LuonnontiedeTiikeri.Joni.html")
+    }
+    if(document.getElementById("vastaus4").checked){
+        window.location.replace("../../Luonnontiede/Kysymys1/infoSivut/LuonnontiedeHuuhkaja.Joni.html")
+    }
+    if(document.getElementById("vastaus5").checked){
+        window.location.replace("../../Luonnontiede/Kysymys1/infoSivut/LuonnontiedeKettu.Joni.html")
+    }
+};
